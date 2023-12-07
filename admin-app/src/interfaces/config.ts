34e9c369dda7a5db2fd65202c39731510d692830
kokweikhong <kokweikhong@gmail.com>
2023-12-07 09:21:21 +0800

    update config

--- admin-app/src/interfaces/config.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const ConfigSchema = z.object({
-  mainServiceURL: z.string(),
-  apiURL: z.string(),
-  MainServiceURL: z.string(),
-});
-
-export type Config = z.infer<typeof ConfigSchema>;
--- admin-app/src/lib/config.ts
+++ admin-app/src/lib/config.ts
@@ -1,11 +1,17 @@
-import { Config } from "@/interfaces/config";
+import { z } from "zod";
+
+const ConfigSchema = z.object({
+  mainServiceURL: z.string(),
+});
+
+type Config = z.infer<typeof ConfigSchema>;
 
 export const getConfig = (): Config => {
   return {
-    mainServiceURL: process.env.NEXT_PUBLIC_MAIN_SERVICE_URL + "/api/v1" || "http://localhost:3000/api",
-    apiURL: process.env.NEXT_PUBLIC_MAIN_SERVICE_URL || "http://localhost:3000/api",
-    MainServiceURL: process.env.NEXT_PUBLIC_MAIN_SERVICE_URL || "http://localhost:3000/api",
+    mainServiceURL:
+      process.env.NEXT_PUBLIC_MAIN_SERVICE_URL + "/api/v1" ||
+      "http://localhost:3000/api",
   };
-}
+};
 
 export const config = getConfig();
